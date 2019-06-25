34b2a5758ced850f1ddcf2d3aa7745e8fffc2e1d
Saghir Bashir <saghir@ilustat.com>
2019-06-25 18:10:26 +0100

    Added Introduction to Rmarkdown slides

--- _make.R
+++ _make.R
@@ -9,3 +9,6 @@ here()
 
 download.file("https://raw.githubusercontent.com/saghirb/Getting-Started-with-Git-and-GitHub-for-R-Users/master/Presentation/Getting-Started-with-Git-and-GitHub-for-R-Users.html",
               here("shared", "Getting-Started-with-Git-and-GitHub-for-R-Users.html"))
+
+download.file("https://raw.githubusercontent.com/saghirb/Rmarkdown-Intro-Workshop/master/Presentation/Rmarkdown-Intro-Workshop.html",
+              here("shared", "Rmarkdown-Intro-Workshop.html"))
